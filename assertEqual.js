2ec14190ac1015830bd7a0966004e2905b3289cf
Gerard Compion <gcompion93@gmail.com>
2022-01-10 15:48:48 -0500

    simple function to replace console.assert with something flashier

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`😎😎😎 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤢🤢🤢 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual('Lighthouse Labs', "Bootcamp");
+assertEqual(1, 1);
+assertEqual(4, 5);
+assertEqual(3.3, 3.3);
+assertEqual('dog', 'cat');
+assertEqual('fish', 'fish');

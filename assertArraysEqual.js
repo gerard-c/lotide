cd8b39479299700ee20f8f7dea29d804016acac5
Gerard Compion <gcompion93@gmail.com>
2022-01-11 15:16:48 -0500

    fixed some wording

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,14 @@
-const eqArrays = (arr1, arr2) => {
+const assertArraysEqual = (arr1, arr2) => {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       console.log(`🤢🤢🤢 Assertion Failed: ${arr1} and ${arr2} are not equal`)
-      return; //"break" and "continue" both allow the pass message to print and "process.exit()" prevents future test calls
+      return;
     }
   }
   console.log(`😎😎😎 Assertion Passed: ${arr1} and ${arr2} are equal`);
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]);
-eqArrays([1, 2, 3], [1, 2, '3']);
-eqArrays(['1', 2, '3'], ['1', 2, '3']);
-eqArrays([1, 2, 3], [2, 1, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, '3']);
+assertArraysEqual(['1', 2, '3'], ['1', 2, '3']);
+assertArraysEqual([1, 2, 3], [2, 1, 3]);

df2b3edcec8a9761f66e60172dd1ac69d47768cd
Gerard Compion <gcompion93@gmail.com>
2022-01-12 17:28:49 -0500

    added logic to compare arrays within objects using eqArrays

--- eqObjects.js
+++ eqObjects.js
@@ -6,8 +6,18 @@ const assertEqual = (actual, expected) => {
   }
 };
 
-// returns true if both objects have identical keys with identical values
-// otherwise it memes on you :(
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 
 const eqObjects = function(object1, object2) {
   const keys1 = Object.keys(object1);
@@ -16,6 +26,9 @@ const eqObjects = function(object1, object2) {
     return false;
   } else {
     for (key of keys1) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        return eqArrays(object1[key], object2[key]);
+      }
       if (object1[key] !== object2[key]) {
         return false;
       }
@@ -24,9 +37,9 @@ const eqObjects = function(object1, object2) {
   }
 };
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log(eqObjects(cd, dc)); // => true
 
-const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
+const cd2 = { c: "1", d: ["2", 3, 4] };
+console.log(eqObjects(cd, cd2)); // => false

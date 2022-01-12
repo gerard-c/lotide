db340536fdef1069cbf787a407bcba26f248b08f
Gerard Compion <gcompion93@gmail.com>
2022-01-12 13:43:58 -0500

    added for...of loop to function and confirmed that it goe through letters of input string

---
+++ countLetters.js
@@ -0,0 +1,17 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`😎😎😎 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤢🤢🤢 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const results = {}
+
+  for (const letter of sentence) {
+    console.log(letter);
+  }
+};
+
+countLetters('elephant');

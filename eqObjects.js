2e45a583879a65e9d25065c5b1908dbb322cdf1a
Gerard Compion <gcompion93@gmail.com>
2022-01-12 17:06:13 -0500

    function seperates keys of each object and compares their values to check for an early failure

--- eqObjects.js
+++ eqObjects.js
@@ -5,3 +5,22 @@ const assertEqual = (actual, expected) => {
     console.log(`🤢🤢🤢 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+// returns true if both objects have identical keys with identical values
+// otherwise it memes on you :(
+
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+eqObjects(ab, ba); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+eqObjects(ab, abc); // => false

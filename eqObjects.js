e1b681568ba023eab59633af5c2243e40b4ea535
Gerard Compion <gcompion93@gmail.com>
2022-01-12 17:38:15 -0500

    final commit

--- eqObjects.js
+++ eqObjects.js
@@ -39,7 +39,14 @@ const eqObjects = function(object1, object2) {
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
+assertEqual(eqObjects(cd, dc), true);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
+assertEqual(eqObjects(cd, cd2), false);
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true);
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false);

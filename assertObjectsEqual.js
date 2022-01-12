96b4920511c810ffac807d269a8ada6c9e156b3c
Gerard Compion <gcompion93@gmail.com>
2022-01-12 17:54:04 -0500

    integrated eqArrays and replaced mentions of objects with actual/expected

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,34 +1,31 @@
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  const eqArrays = (arr1, arr2) => {
+    if (arr1.length !== arr2.length) {
       return false;
     }
-  }
-  return true;
-};
-
-const eqObjects = function(object1, object2) {
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  };
+  const keys1 = Object.keys(actual);
+  const keys2 = Object.keys(expected);
   if (keys1.length !== keys2.length) {
     return false;
   } else {
     for (key of keys1) {
-      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        return eqArrays(object1[key], object2[key]);
+      if (Array.isArray(actual[key]) && Array.isArray(expected[key])) {
+        return eqArrays(actual[key], expected[key]);
       }
-      if (object1[key] !== object2[key]) {
+      if (actual[key] !== expected[key]) {
         return false;
       }
     }
     return true;
   }
 };
-
-const assertObjectsEqual = function(actual, expected) {
-  const inspect = require('util').inspect;
-  
-}

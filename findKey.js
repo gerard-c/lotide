428ff178a35d61e0a8113e5db611d689afcff7e4
Gerard Compion <gcompion93@gmail.com>
2022-01-13 16:10:43 -0500

    findKey is able to return the desired result

---
+++ findKey.js
@@ -0,0 +1,25 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`😎😎😎 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤢🤢🤢 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (object, callback) => {
+  const keys = Object.keys(object);
+  for (const key of keys) {
+    if (callback(object[key]) === true) {
+      return key;
+    }
+  }
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2), 'noma');

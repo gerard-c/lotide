c75bf2259701fdbcec0221e95d49625daa5e0cf8
Gerard Compion <gcompion93@gmail.com>
2022-01-13 16:21:13 -0500

    final commit

--- findKey.js
+++ findKey.js
@@ -23,3 +23,28 @@ assertEqual(findKey({
   "Ora": { stars: 2 },
   "Akelarre": { stars: 3 }
 }, x => x.stars === 2), 'noma');
+
+assertEqual(findKey({
+  'Vanilla': {level: 60},
+  'TBC': {level: 70},
+  'Wrath': {level: 80},
+  'Cataclysm': {level: 85},
+  'MoP': {level: 90},
+  'WoD': {level: 100}
+}, x => x.level === 80), 'Wrath');
+
+assertEqual(findKey({
+  'me': {age: 28},
+  'mom': {age: 61},
+  'dad': {age: 65},
+  'brother': {age: 31}
+}, x => x.age === 31), 'brother');
+
+assertEqual(findKey({
+  'arbitrary': {value: 66},
+  'meaningless': {value: 42},
+  'excessive': {value: 69},
+  'irrelevent': {value: 666},
+  'needless': {value: 420},
+  'unnecessary': {value: 4}
+}, x => x.value === 43), undefined);

c40a72f2274c9653c4988c79a3eed56dff44f45a
Gerard Compion <gcompion93@gmail.com>
2022-01-13 14:58:19 -0500

    completed logic of map function

--- map.js
+++ map.js
@@ -3,9 +3,7 @@ const words = ["ground", "control", "to", "major", "tom"];
 const map = (array, callback) => {
   const results = []
   for (let item of array) {
-    console.log('item BEFORE ', item);
-    console.log('item AFTER: ', callback(item));
-    console.log('---');
+    results.push(callback(item));
   }
   return results;
 }

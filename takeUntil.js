373d7dcf6f7b28ea0a0df5ae2927296515823ac8
Gerard Compion <gcompion93@gmail.com>
2022-01-13 15:51:29 -0500

    final commit

--- takeUntil.js
+++ takeUntil.js
@@ -1,5 +1,15 @@
+const assertArraysEqual = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log(`🤢🤢🤢 Assertion Failed: ${arr1} and ${arr2} are not equal`);
+      return;
+    }
+  }
+  console.log(`😎😎😎 Assertion Passed: ${arr1} and ${arr2} are equal`);
+};
+
 const takeUntil = (array, callback) => {
-  let result = []
+  let result = [];
   for (const item of array) {
     if (!callback(item)) {
       result.push(item);
@@ -8,14 +18,19 @@ const takeUntil = (array, callback) => {
     }
   }
   return result;
-}
+};
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+assertArraysEqual(takeUntil([1, 2, 5, 7, 2, -1, 2, 4, 5], x => x < 0), [1, 2, 5, 7, 2]);
+assertArraysEqual(takeUntil(["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"], x => x === ','), ["I've", 'been', 'to', 'Hollywood']);
+assertArraysEqual(takeUntil([15, 17, 91, 32, 42, 34, 56], x => x === 42), [15, 17, 91, 32]);
+assertArraysEqual(takeUntil(['fish', 45, 5.5, 'pig', true, 'string', 99], x => typeof x === 'boolean'), ['fish', 45, 5.5, 'pig']);
 
-console.log('---');
+// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+// const results1 = takeUntil(data1, x => x < 0);
+// console.log(results1);
 
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+// console.log('---');
+
+// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+// const results2 = takeUntil(data2, x => x === ',');
+// console.log(results2);

86bb4301e423da29851bf1977a2d6dde130656c0
Gerard Compion <gcompion93@gmail.com>
2022-01-13 15:13:48 -0500

    final commit

--- map.js
+++ map.js
@@ -1,4 +1,12 @@
-const words = ["ground", "control", "to", "major", "tom"];
+const assertArraysEqual = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log(`🤢🤢🤢 Assertion Failed: ${arr1} and ${arr2} are not equal`)
+      return;
+    }
+  }
+  console.log(`😎😎😎 Assertion Passed: ${arr1} and ${arr2} are equal`);
+};
 
 const map = (array, callback) => {
   const results = []
@@ -8,6 +16,7 @@ const map = (array, callback) => {
   return results;
 }
 
-const results = map(words, word => word[0]);
-
-console.log(results);
+assertArraysEqual(map([1, 2, 3, 4], num => num * 2), [2, 4, 6, 8]);
+assertArraysEqual(map(['cat', 'dog', 'tree', 'house'], word => word[0]), ['c', 'd', 't', 'h']);
+assertArraysEqual(map([3, 6, 9, 12], num => num + num), [6, 12, 18, 24]);
+assertArraysEqual(map(['lamp', 'fish', 'bird', 'cage'], word => word.toUpperCase()), ['LAMP', 'FISH', 'BIRD', 'CAGE']);

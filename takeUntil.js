ede35eedcd7bd645cf19fa04ef979bada3f067b8
Gerard Compion <gcompion93@gmail.com>
2022-01-13 15:38:42 -0500

    completed logic of takeUntil function

--- takeUntil.js
+++ takeUntil.js
@@ -1,7 +1,11 @@
 const takeUntil = (array, callback) => {
   let result = []
   for (const item of array) {
-    console.log(callback(item));
+    if (!callback(item)) {
+      result.push(item);
+    } else {
+      break;
+    }
   }
   return result;
 }
@@ -9,3 +13,9 @@ const takeUntil = (array, callback) => {
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);

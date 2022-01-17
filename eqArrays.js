049ddfaebaf2ade4b10e20d606fbfcbc8b38a6dc
Gerard Compion <gcompion93@gmail.com>
2022-01-17 14:10:27 -0500

    refactored eqArrays to be exportable and setup test cases for eqArrays using assertEqual in a new file

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`😎😎😎 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🤢🤢🤢 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = (arr1, arr2) => {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
@@ -15,7 +7,4 @@ const eqArrays = (arr1, arr2) => {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
